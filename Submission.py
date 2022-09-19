77a5eddbc447b8d39b06487b2dde32043994f535
Aryanexe <95134600+Aryanexe@users.noreply.github.com>
2022-09-19 15:16:35 +0530

    Update Submission.py

--- Submission.py
+++ Submission.py
@@ -26,14 +26,29 @@ print(list2)
 list2.remove(34)
 print(list2)
 
+OUTPUT: 
+              ['hello', 'this is', 'a list']
+              [362.7, 43, 'World', True]
+              ['hello', 'this is', 'a list', 362.7, 43, 'World', True]
+              43
+              trampoline
+              [362.7, 34, 'trampoline', True, 69]
+              [362.7, 'inserted', 34, 'trampoline', True, 69]
+              [362.7, 'inserted', 34, 'trampoline', True]
+              [362.7, 'inserted', 'trampoline', True]
 
+              
 #tuple example
 tuple1=(["data", 23, True, 42.46])
 print(tuple1)
 tuple2=(["second", 54])
 print(tuple1,tuple2)
 
+OUTPUT: 
+       ['data', 23, True, 42.46]
+       ['data', 23, True, 42.46] ['second', 54]
 
+       
 #Dictionary example
 dict={
        "Name":["Aryan","Sihag"],
@@ -53,6 +68,12 @@ print(dict)
 del dict["OS"]
 print(dict)
 
+OUTPUT:
+       ['Aryan', 'Sihag'] Windows
+       {'Name': ['Aryan', 'Sihag'], 'University': 'BITS Pilani', 'OS': 'Windows', 'RAM': '8GB', 'version': 7}
+       {'Name': 'Jojo', 'University': 'BITS Pilani', 'OS': 'Windows', 'RAM': '8GB', 'version': 7}
+       {'Name': 'Jojo', 'University': 'BITS Pilani', 'RAM': '8GB', 'version': 7}
+
 
 #slicing
 var="Engineering"
@@ -62,10 +83,23 @@ print(var[:4])
 print(var[-6:-1])
 print(var[0:100])
 
+OUTPUT:
+       Engine
+       ineering
+       Engi
+       eerin
+       Engineering
+
 
 #step index (negative and positive both)
 print(var[1:8:2])
 print(var[::-2])
 print(var[8:1:-2])
 print(var[10::-3])
+
+OUTPUT:
+       nier
+       giengE
+       ieng
+       grnn
 
